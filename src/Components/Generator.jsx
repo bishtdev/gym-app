c2986ff2d263c1258dbbf1596206dc8267328e55
bishtdev <bishtdev030@gmail.com>
2024-06-18 23:13:32 +0530

    added ultimate objective section

--- src/Components/Generator.jsx
+++ src/Components/Generator.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import SectionWrapper from "./SectionWrapper";
-import { WORKOUTS } from "../utlis/swoldiers";
+import { SCHEMES, WORKOUTS } from "../utlis/swoldiers";
 import { FaCaretDown } from "react-icons/fa";
 
 function Header(props) {
@@ -19,13 +19,15 @@ function Header(props) {
 }
 
 export default function Generator() {
-  const [toggle, setToggle] = useState("false");
+  const [showModal, setShowModal] = useState(false);
+  const [poison, setPoison] = useState('individual')
+  const [muscles, setMuscles] = useState([])
+  const [goals, setGoals] = useState('strength_power')
 
-  const handleToggle = () => {
-    return !toggle;
+  const toggleModal = () => {
+    setShowModal(!showModal)
   };
 
-  const showModal = false;
 
   return (
     <SectionWrapper
@@ -41,7 +43,10 @@ export default function Generator() {
         {Object.keys(WORKOUTS).map((type, typeIndex) => {
           return (
             <button
-              className="bg-slate-950 border-2 border-blue-500 py-3 rounded-lg hover:border-blue-700"
+              onClick={()=>{setPoison(type), console.log(type, poison);}}
+                className={'bg-slate-950 border-2  duration-200 px-4 hover:border-blue-700 py-3 rounded-lg ' 
+                  + (type === poison ? ' border-blue-700' : ' border-blue-400')}
+               
               key={typeIndex}
             >
               <p className="font-bold  ">{type.replaceAll("_", " ")}</p>
@@ -49,17 +54,39 @@ export default function Generator() {
           );
         })}
       </div>
+
       <Header
         index={"02"}
         title={"Lock on targets"}
         description={"Select Muscles for annihilation"}
       />
-      <div className="bg-slate-950 p-3 border border-solid border-blue-400 rounded-lg">
-        <button className="relative p-3 flex items-center justify-center">
+      <div className="bg-slate-950 p-3 border border-solid border-blue-400 rounded-lg flex flex-col">
+        <button onClick={toggleModal} className="relative p-3 flex items-center justify-center">
           <p>Select muscle groups</p>
           <FaCaretDown className="absolute right-3 top-1/2 -translate-y-1/2 " />
         </button>
         {showModal && <div>modal</div>}
+      </div>
+
+      <Header
+        index={"03"}
+        title={"Become Juggernaut"}
+        description={"Select you ultimate objective"}
+      />
+      <div className="grid grid-cols-3 sm:grid-cols-3 gap-4">
+        {Object.keys(SCHEMES).map((scheme, schemeIndex) => {
+          return (
+            <button
+              onClick={()=>{setGoals(scheme)}}
+                className={'bg-slate-950 border-2  duration-200 px-4 hover:border-blue-700 py-3 rounded-lg ' 
+                  + (scheme === goals ? ' border-blue-700' : ' border-blue-400')}
+               
+              key={schemeIndex}
+            >
+              <p className="font-bold  ">{scheme.replaceAll("_", " ")}</p>
+            </button>
+          );
+        })}
       </div>
     </SectionWrapper>
   );
